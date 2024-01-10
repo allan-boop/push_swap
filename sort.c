1e242d3d744afd995b5f830a6ac14859516e8d67
Allan Hans <allan-boop>
2024-01-10 17:35:21 +0100

    ok pour 100 environs 1150cp

--- sort.c
+++ sort.c
@@ -6,36 +6,37 @@
 /*   By: ahans <ahans@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:39:24 by ahans             #+#    #+#             */
-/*   Updated: 2024/01/10 17:06:33 by ahans            ###   ########.fr       */
+/*   Updated: 2024/01/10 17:34:10 by ahans            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	rotate_or_reverse(t_stack **stack, int *tab, int var, int step)
+int	dir(t_stack **stack, int *tab)
 {
 	int		i;
-	int		x;
+	int		res;
 	t_stack	*tmp;
 
+	i = 0;
+	res = 100;
 	tmp = *stack;
-	i = 1;
-	x = 0;
-	while (i < stack_len(stack))
+	while (tmp->index != tab[0] && tmp->index != tab[1] && tmp->index != tab[2])
 	{
-		if (tmp->index == tab[0] || tmp->index == tab[1]
-			|| tmp->index == tab[2])
-		{
-			x += i;
-		}
 		tmp = tmp->next;
 		i++;
 	}
-	x -= var;
-	x /= step;
-	if (x <= stack_len(stack) / 2)
-		return (1);
-	return (0);
+	res = i;
+	i = 0;
+	tmp = *stack;
+	while (tmp->index != tab[0] && tmp->index != tab[1] && tmp->index != tab[2])
+	{
+		tmp = tmp->prev;
+		i++;
+	}
+	if (i < res)
+		return (0);
+	return (1);
 }
 
 void	sort(t_stack **stack_a, t_stack **stack_b)
@@ -84,7 +85,7 @@ void	sort(t_stack **stack_a, t_stack **stack_b)
 				step--;
 				pos = *stack_a;
 			}
-			else if (rotate_or_reverse(&pos, tab, var, step) == 1)
+			else if (dir(&pos, tab) == 1)
 				ra(stack_a);
 			else
 				rra(stack_a);
